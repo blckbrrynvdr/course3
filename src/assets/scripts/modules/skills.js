4676caa1d5b174bb8fc613b61d8f9682ccada17a
Artur <v8a8rt@gmail.com>
2019-01-07 16:53:01 +0300

    работай же блин

---
+++ src/assets/scripts/modules/skills.js
@@ -0,0 +1,50 @@
+import Vue from 'vue';
+
+const skill = {
+  template: "#skill",
+  props: {
+    skillPercentage: Number,
+    skillTitle: String
+  },
+  methods: {
+    drawCircleDependsOnPercentage() {
+      const circle = this.$refs["circle"];
+      const dashOffset = parseInt(
+        getComputedStyle(circle).getPropertyValue("stroke-dashoffset")
+      );
+  
+      const percent = (dashOffset / 100) * (100 - this.skillPercentage);
+  
+      circle.style.strokeDashoffset = percent;
+    }
+  },
+  mounted() {
+    this.drawCircleDependsOnPercentage();
+  }
+}
+
+const skillsRow = {
+  template: "#skills-item",
+  props: {
+    skillsGroupObj: Object
+  },
+  components: {
+    skill
+  }
+}
+
+new Vue ({
+  el: "#skills-component",
+  components: {
+    skillsRow
+  },
+  data() {
+    return {
+      skills: {}
+    }
+  },
+  created() {
+    this.skills = require('../../../data/skillls.json');
+  },
+  template: "#skills-list",
+});
